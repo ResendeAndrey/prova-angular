5a6a41ffd403b1825f6f702759dbabf723259929
AndreyCNF <resende.andrey@yahoo.com.br>
2020-05-04 17:35:11 -0300

    Desafio Andrey Resende

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
+
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+import { CepService } from './cep.service';
+
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,7 +20,18 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+    	imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatTableModule,
+        MatInputModule,
+        HttpClientModule,
+      ],
+      providers: [CepService],
     }).compileComponents();
+    
+    
   }));
 
   it('should create the app', () => {
@@ -21,11 +45,34 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('teste-helpper');
   });
-
-  it('should render title', () => {
+    
+  it('Deve verificar a quantidade de itens no columns', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('teste-helpper app is running!');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.columns.length).toEqual(9)
   });
+  it('Deve receber um valor true para o array de persons', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.persons).toBeTruthy()
+  });
+  it('Verifica funcition addPerson', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.addPerson()).toBeUndefined()
+  }); 
+
+  it('Verifica funcition editPerson', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.editPerson('person')).toBeUndefined()
+  }); 
+  it('Verifica function deletePerson', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    expect(app.changeCep.length).toEqual(1)
+  })
+
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CepService } from './cep.service';
+import { CrudService } from './crud.service';
 
 @Component({
 	selector: 'app-root',
@@ -34,8 +35,8 @@ export class AppComponent {
 	}
 
 	deletePerson(person) {
-		remove(person)
-		this.persons = JSON.parse(get())
+		this.crud.remove(person)
+		this.persons = JSON.parse(this.crud.get())
 	}
 
 	changeCep(event) {
@@ -76,83 +77,19 @@ export class AppComponent {
 		if (error) {
 			alert('Erro!\nPreencha todos os campos!')
 		} else {
-			save(person)
-			this.persons = JSON.parse(get())
+			this.crud.save(person)
+			this.persons = JSON.parse(this.crud.get())
 			this.selectedPerson = null
 		}
 	}
 
 	ngOnInit() {
-		if (!get() || !JSON.parse(get()).length) populateTable()
-		this.persons = JSON.parse(get())
+		if (!this.crud.get() || !JSON.parse(this.crud.get()).length) this.crud.populateTable()
+		this.persons = JSON.parse(this.crud.get())
 	}
 
-	constructor(public cep: CepService) { }
+	constructor(public cep: CepService, public crud: CrudService) { }
 }
 
-function populateTable() {
-	var persons = [
-		{
-			name: 'Maria Flores',
-			cpf: '83321492075',
-			phone: '1533283928',
-			email: 'maria_f@gmail.com',
-			cep: '69906043',
-			state: 'AC',
-			city: 'Rio Branco',
-			street: 'Rua Arnaldo Aleixo (Conjunto Canaã)',
-		},
-		{
-			name: 'João Carlos',
-			cpf: '31213393035',
-			phone: '1532841040',
-			email: 'joao.c@gmail.com',
-			cep: '79096766',
-			state: 'MS',
-			city: 'Campo Grande',
-			street: 'Rua Rodolfho José Rospide da Motta',
-		},
-		{
-			name: 'Stephanie Dias',
-			cpf: '02085196020',
-			phone: '1533294040',
-			email: 'ste.dias@gmail.com',
-			cep: '23825080',
-			state: 'RJ',
-			city: 'Itaguaí',
-			street: 'Rua Mario Bastos Filho',
-		},
-		{
-			name: 'Mirtes Souza',
-			cpf: '33764389001',
-			phone: '1530178756',
-			email: 'irma.mirtes@gmail.com',
-			cep: '40420150',
-			state: 'BA',
-			city: 'Salvador',
-			street: '1ª Travessa Clóvis de Almeida Maia',
-		}
-	]
 
-	localStorage.setItem('persons', JSON.stringify(persons))
-}
 
-function get() {
-	return localStorage.getItem('persons')
-}
-
-function save(person) {
-	var persons = JSON.parse(localStorage.getItem('persons'))
-	var index = persons.findIndex(foundPerson => Number(foundPerson.cpf) == Number(person.cpf))
-	if (index == -1) index = persons.length
-	persons[index] = person
-	localStorage.setItem('persons', JSON.stringify(persons))
-}
-
-function remove(person) {
-	var persons = JSON.parse(localStorage.getItem('persons'))
-	var cpf = Number(person.cpf)
-	var index = persons.findIndex(foundPerson => foundPerson.cpf == String(cpf))
-	persons.splice(index, 1)
-	localStorage.setItem('persons', JSON.stringify(persons))
-}
